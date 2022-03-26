2fd733c177dac2f59bd7347e5cab2c6ce3e3eab2
MatishaKansal <64193940+MatishaKansal@users.noreply.github.com>
2022-03-26 11:30:45 +0530

    Add files via upload

--- App.py
+++ App.py
@@ -21,6 +21,14 @@ planet_data_1 = data_1[1:]
 headers_2 = data_2[0]
 planet_data_2 = data_2[1:]
 
+temp_planet_data_2 = list(planet_data_2)
+
+for planet_data in temp_planet_data_2:
+  planet_radius = planet_data[4]
+  if planet_radius.lower() == 'n/a':
+    planet_data_2.remove(planet_data)
+
+    
 headers = headers_1 + headers_2
 planet_data = []
 
@@ -31,3 +39,4 @@ with open('planet.csv', 'a+')as f:
     csv_writer = csv.writer(f)
     csv_writer.writerow(headers)
     csv_writer.writerows(planet_data)
+
